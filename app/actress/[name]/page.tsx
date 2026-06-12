ba972f9bc2ad5b8397dd27e67ab4b01bae009095
shutaken <shutaken4257@gmail.com>
2026-06-13 03:28:36 +0900

    Add files via upload

---
+++ app/actress/[name]/page.tsx
@@ -0,0 +1,22 @@
+import Feed from "@/components/Feed";
+
+export const dynamic = "force-dynamic";
+
+type PageProps = {
+  params: Promise<{
+    name: string;
+  }>;
+};
+
+export default async function ActressFeedPage({ params }: PageProps) {
+  const { name } = await params;
+  const actressName = decodeURIComponent(name);
+
+  return (
+    <Feed
+      actressName={actressName}
+      showGenreChips={false}
+      titlePrefix={actressName}
+    />
+  );
+}
---
+++ app/actresses/page.tsx
@@ -0,0 +1,7 @@
+import ActressList from "@/components/ActressList";
+
+export const dynamic = "force-dynamic";
+
+export default function ActressesPage() {
+  return <ActressList />;
+}
